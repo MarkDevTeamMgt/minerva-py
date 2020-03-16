0295fd8b3f20009bc354378a47913946f3f5f513
hulingfei <hulingfei84@gmail.com>
2020-03-16 17:26:19 -0400

    init commit

---
+++ tests/test_fetch.py
@@ -0,0 +1,14 @@
+from minerva import Client
+
+
+def test_fetch_data():
+    client = Client(user='test@moyi-tech.org', password='moyi1234')
+    data = client.fetch(
+        'marketdata?sector=information_technology&begin=2020-01-10&end=2020-02-20&start=0&size=20')
+
+    assert len(data['hits']) == 20
+
+    data = client.fetch(
+        'fundamentaldata?&sector=information_technology&sheet=balance&begin=2018-03-10&end=2020-03-10&start=0&size=10')
+
+    assert len(data['hits']) > 0
